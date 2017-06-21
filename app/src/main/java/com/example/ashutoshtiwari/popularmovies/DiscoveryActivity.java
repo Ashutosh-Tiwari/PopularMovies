0af7379941d81a68b508acdcad69ca7c2a344c4d
Ashutosh.tiwari <ashutiwari9518@gmail.com>
2017-06-21 20:07:05 +0530

    Added Random Images to Grid

--- app/src/main/java/com/example/ashutoshtiwari/popularmovies/DiscoveryActivity.java
+++ app/src/main/java/com/example/ashutoshtiwari/popularmovies/DiscoveryActivity.java
@@ -4,14 +4,32 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.GridView;
 
+import java.util.ArrayList;
+
 public class DiscoveryActivity extends AppCompatActivity {
+
+    private GridView gridView;
+    private ArrayList<Integer> images;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_discovery);
 
-        GridView gridView = (GridView) findViewById(R.id.gridview);
-        gridView.setAdapter(new ImageAdapter(this));
+        updateUI();
+        gridView = (GridView) findViewById(R.id.gridview);
+        gridView.setAdapter(new ImageAdapter(this, images));
+    }
+
+    private void updateUI() {
+        images = new ArrayList<>();
+        images.add(R.drawable.sample_0);
+        images.add(R.drawable.sample_1);
+        images.add(R.drawable.sample_2);
+        images.add(R.drawable.sample_3);
+        images.add(R.drawable.sample_4);
+        images.add(R.drawable.sample_5);
+        images.add(R.drawable.sample_6);
+        images.add(R.drawable.sample_7);
     }
 }
--- app/src/main/java/com/example/ashutoshtiwari/popularmovies/ImageAdapter.java
+++ app/src/main/java/com/example/ashutoshtiwari/popularmovies/ImageAdapter.java
@@ -1,11 +1,12 @@
 package com.example.ashutoshtiwari.popularmovies;
 
-import android.content.Context;
+import android.app.Activity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.GridView;
 import android.widget.ImageView;
+
+import java.util.ArrayList;
 
 /**
  * Created by Ashutosh.tiwari on 21/06/17.
@@ -13,50 +14,42 @@ import android.widget.ImageView;
 
 class ImageAdapter extends BaseAdapter {
 
-    private Context context;
+    private Activity activity;
+    private ArrayList<Integer> images;
 
-    private Integer[] resources = {
-            R.drawable.sample_0,
-            R.drawable.sample_1,
-            R.drawable.sample_2,
-            R.drawable.sample_3,
-            R.drawable.sample_4,
-            R.drawable.sample_5,
-            R.drawable.sample_6,
-            R.drawable.sample_7
-    };
-
-    ImageAdapter(Context context) {
-        this.context = context;
+    ImageAdapter(Activity activity, ArrayList<Integer> images) {
+        this.activity = activity;
+        this.images = images;
     }
 
     @Override
     public int getCount() {
-        return resources.length;
+        return images.size();
     }
 
     @Override
-    public Object getItem(int i) {
-        return null;
+    public Object getItem(int position) {
+        return images.get(position);
     }
 
     @Override
-    public long getItemId(int i) {
+    public long getItemId(int position) {
         return 0;
     }
 
+
     @Override
-    public View getView(int position, View view, ViewGroup viewGroup) {
+    public View getView(int position, View convertView, ViewGroup viewGroup) {
+
         ImageView imageView;
-        if (view == null) {
-            imageView = new ImageView(context);
-            imageView.setLayoutParams(new GridView.LayoutParams(100, 100));
-            imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+        if (convertView == null) {
+            imageView = new ImageView(activity);
             imageView.setPadding(0, 0, 0, 0);
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
         } else {
-            imageView = (ImageView) view;
+            imageView = (ImageView) convertView;
         }
-        imageView.setImageResource(resources[position]);
+        imageView.setImageResource(images.get(position));
         return imageView;
     }
 }

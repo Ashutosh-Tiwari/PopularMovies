fc2ea5d5d161712cc190b375a3472c510564ed35
Ashutosh.tiwari <ashutiwari9518@gmail.com>
2017-06-22 20:59:22 +0530

    Implimented Retrofit to fetch movies data in JSON Added Detail Activity

--- app/src/main/java/com/example/ashutoshtiwari/popularmovies/DiscoveryActivity.java
+++ app/src/main/java/com/example/ashutoshtiwari/popularmovies/DiscoveryActivity.java
@@ -1,10 +1,22 @@
 package com.example.ashutoshtiwari.popularmovies;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class DiscoveryActivity extends AppCompatActivity {
 
@@ -19,17 +31,40 @@ public class DiscoveryActivity extends AppCompatActivity {
         updateUI();
         gridView = (GridView) findViewById(R.id.gridview);
         gridView.setAdapter(new ImageAdapter(this, images));
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(DiscoveryActivity.this, MovieDetailActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     private void updateUI() {
         images = new ArrayList<>();
-        images.add(R.drawable.sample_0);
-        images.add(R.drawable.sample_1);
-        images.add(R.drawable.sample_2);
-        images.add(R.drawable.sample_3);
-        images.add(R.drawable.sample_4);
-        images.add(R.drawable.sample_5);
-        images.add(R.drawable.sample_6);
-        images.add(R.drawable.sample_7);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://api.themoviedb.org/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        TMDClient tmdClient = retrofit.create(TMDClient.class);
+        Call<MoviesData> call = tmdClient.getMovies("ReplaceWithAPIKey");
+        call.enqueue(new Callback<MoviesData>() {
+            @Override
+            public void onResponse(@NonNull Call<MoviesData> call, @NonNull Response<MoviesData> response) {
+                MoviesData body = response.body();
+                //Log.v("DiscoveryActivity.java", "Response: " + body.getResults().toString());
+                //body.getResults().get(0).getGenreIds().get(0);
+                //ArrayList<String> moviesThumbnailPath = body.getResults().
+
+            }
+
+            @Override
+            public void onFailure(Call<MoviesData> call, Throwable t) {
+                Log.d("Discovery", t.toString());
+                Toast.makeText(getApplicationContext(), "Something went wrong", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/ashutoshtiwari/popularmovies/TMDClient.java
@@ -0,0 +1,16 @@
+package com.example.ashutoshtiwari.popularmovies;
+
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+
+/**
+ * Created by Ashutosh.tiwari on 22/06/17.
+ */
+
+public interface TMDClient {
+    @GET("3/discover/movie")
+    Call<MoviesData> getMovies(@Query("api_key") String key);
+}
